9e3721997ba8083dfeb08efd5c15ae5d26b49d3c
Zakariya Oulhadj <zakariyaoulhadj01@gmail.com>
2019-11-13 03:25:14 +0000

    changes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
+$(document).ready(() => {
 	// Run when window is scrolling
 	// Changed the navbar background color
-	$(window).scroll(function() {
+	$(window).scroll(() => {
   		let scroll = $(window).scrollTop();
 		if (scroll > 80) {
 			$("header").css("transition", ".2s");
@@ -13,12 +13,12 @@ $(document).ready(function() {
 	})
 
 	// Expand space facts card on button click
-	$(".expand").click(function () {
+	$(".expand").click(() => {
 		$(".card").toggleClass('card-expanded');
 		$(".expand").css("display", "none");
 		$(".collapse").css("display", "block");
 	});
-	$(".collapse").click(function () {
+	$(".collapse").click(() => {
 		$(".card").toggleClass('card-expanded');
 		$(".expand").css("display", "block");
 		$(".collapse").css("display", "none");
